77e58073e88889c95c03671dab99ef76c01a2e19
makerbox <mattwerth@mattwerth.com>
2015-01-17 15:43:12 +1100

    mail notifications

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,3 +1,19 @@
 class NotificationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "info@notjustadate.com"
+
+  def signup_receipt(user)
+  	@user = user
+  	mail(to: @user.email, subject: 'Welcome to Not Just a Date')
+  end
+
+  def purchase_notification(user, purchase)
+  	@user = user
+  	mail(to: @user.email, subject: 'New purchase from Not Just a Date')
+  end
+
+  def purchase_receipt(user, purchase)
+  	@user = user
+  	mail(to: @user.email, subject: 'Your Not Just a Date booking')
+  end
+
 end
